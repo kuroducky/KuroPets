e7ff06056fc107aefbd289b0caef9f2ce29e5cdc
Wonn Jen <lee.wonnjen@gmail.com>
2020-03-05 17:29:08 +0800

    Changes parameters name

--- Queries/account.js
+++ Queries/account.js
@@ -21,23 +21,23 @@ const getUserById = (request, response) => {
 }
 
 const createUser = (request, response) => {
-    const { name, password, phone } = request.body;
+    const { accountName, accountPassword, accountPhone } = request.body;
 
-    pool.query('INSERT INTO "tbl_Account" ("accountName", "accountPassword", "accountPhone") VALUES ($1, $2, $3)', [name, password, phone], (error, results) => {
+    pool.query('INSERT INTO "tbl_Account" ("accountName", "accountPassword", "accountPhone") VALUES ($1, $2, $3) RETURNING *', [name, password, phone], (error, results) => {
         if (error) {
             throw error;
         }
-        response.status(200).send(`User added with ID: ${result.rows}`);
+        response.status(200).send(`User added with ID: ${result.rows[0].accountID}`);
     });
 }
 
 const updateUser = (request, response) => {
     const id = parseInt(request.params.id);
-    const { name, password, phone } = request.body;
+    const { accountName, accountPassword, accountPhone } = request.body;
 
     pool.query(
-        'UPDATE "tbl_Account" SET "accountName" = $1, "accountPassword" = $2, "accountPhone" = $3 WHERE "accountID" = $4',
-        [name, password, phone, id],
+        'UPDATE "tbl_Account" SET "accountName" = $1, "accountPassword" = $2, "accountPhone" = $3 WHERE "accountID" = $4 RETURNING *',
+        [accountName, accountPassword, accountPhone, id],
         (error, results) => {
             if (error) {
                 throw error;
